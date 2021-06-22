5ebf8a2072abb2e65db2c3e5f61b203a15161022
tatakharlan <tatakharlan@gmail.com>
2021-06-22 16:49:06 +0300

    BR2JSX

---
+++ br2jsx/components/br2jsx.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './br2jsx.css';
+
+
+class IShop extends React.Component {
+
+    static propTypes = {  
+        text: PropTypes.string.isRequired,      
+    };
+    
+    
+    render() {
+      let text="первый<br>второй<br>третий<br>последний";
+      
+      let cardCode = cardGood.map( v =>
+        <BR2JSX key={v.code} editedGood = {this.state.editedGood} selectedGood = {this.state.selectedGood} cardMode = {this.state.cardMode}
+                goodName= {v.goodName} newGood={this.state.newGood} 
+                cbitemChanged = {this.itemChanged} cbSaved={this.goodSaved}                 
+                goodNameError = {this.state.goodNameError} 
+                countError = {this.state.countError} urlError = {this.state.urlError}
+                priceError = {this.state.priceError} 
+                count= {v.count} code={v.code} price={v.price} url={v.url}  cbCancel={this.EditModeCancel}
+        />      
+      )
+      
+      return <BR2JSX text={text}/>;     
+       
+    }
+  
+  }
+
+  export default IShop;
