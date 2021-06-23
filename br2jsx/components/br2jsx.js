169397d0bbdcf4e7fc317ef2bfd82b33156e452d
tatakharlan <tatakharlan@gmail.com>
2021-06-23 13:53:51 +0300

    BR2JSX ready

--- br2jsx/components/br2jsx.js
+++ br2jsx/components/br2jsx.js
@@ -12,21 +12,18 @@ class BR2JSX extends React.Component {
     
     
     render() {
-      let text="первый<br>второй<br>третий<br>последний";
-      
-      let cardCode = cardGood.map( v =>
-        <BR2JSX key={v.code} editedGood = {this.state.editedGood} selectedGood = {this.state.selectedGood} cardMode = {this.state.cardMode}
-                goodName= {v.goodName} newGood={this.state.newGood} 
-                cbitemChanged = {this.itemChanged} cbSaved={this.goodSaved}                 
-                goodNameError = {this.state.goodNameError} 
-                countError = {this.state.countError} urlError = {this.state.urlError}
-                priceError = {this.state.priceError} 
-                count= {v.count} code={v.code} price={v.price} url={v.url}  cbCancel={this.EditModeCancel}
-        />      
-      )
-      
-      return;     
-       
+      let text = this.props.text;
+      let textArr = text.split(/<br>|<br\/>|<br \/>/);
+      let textArrOut = textArr.map((item, index) => {
+        return <span>{item} <br/></span>;
+      })
+      console.log("result",textArr);
+
+      return(
+        <div className='BR2JSX'>
+          {textArrOut}
+        </div>     
+       )
     }
   
   }
