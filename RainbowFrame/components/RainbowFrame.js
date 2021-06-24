95425e7c54e66d1ba7deb2eac4b9352d903ad139
tatakharlan <tatakharlan@gmail.com>
2021-06-24 14:54:13 +0300

    colors

--- RainbowFrame/components/RainbowFrame.js
+++ RainbowFrame/components/RainbowFrame.js
@@ -13,15 +13,85 @@ class RainbowFrame extends React.Component {
     
     render() {
       let colors = this.props.colors;
-      let colorsArrOut = colors.map((item, index) => {
-        return <div key={index} style={{border:"solid 10px "+item,padding:"10px"}}>
-                    {this.props.children}
-               </div> ;
+      let colorsArrOut;  
+      colorsArrOut = colors.map((item, index) => {                        
+         return (index==0) &&<div key={index} style={{border:"solid 10px "+item,padding:"10px"}}>
+          {
+          colorsArrOut = colors.map((item, index) => {                        
+            return (index==1) &&<div key={index} style={{border:"solid 10px "+item,padding:"10px"}}>
+              
+              {
+          colorsArrOut = colors.map((item, index) => {                        
+            return (index==2) &&<div key={index} style={{border:"solid 10px "+item,padding:"10px"}}>
+                  
+                  {
+              colorsArrOut = colors.map((item, index) => {                        
+                return (index==3) &&<div key={index} style={{border:"solid 10px "+item,padding:"10px"}}>
+                  
+                  {
+              colorsArrOut = colors.map((item, index) => {                        
+                return (index==4) &&<div key={index} style={{border:"solid 10px "+item,padding:"10px"}}>
+                  
+                  {
+              colorsArrOut = colors.map((item, index) => {                        
+                return (index==5) &&<div key={index} style={{border:"solid 10px "+item,padding:"10px"}}>
+                  
+                        {
+                    colorsArrOut = colors.map((item, index) => {                        
+                      return (index==6) &&<div key={index} style={{border:"solid 10px "+item,padding:"10px"}}>
+                        
+                        {this.props.children}
+                        
+                        </div>          
+                    })
+                  }
+                  
+                  </div>          
+              })
+            }
+                  
+                  </div>          
+              })
+            }
+                  
+                  </div>          
+              })
+            }
+              
+              </div>          
+          })
+        }
+              
+              </div>          
+          })
+        }
+          </div>          
       })
+      
+/*
+      colorsArrOut = colors.map((color, n) => {                        
+        colorsArrOut = colors.map((item, index) => {                        
+          return (n) &&<div key={index} style={{border:"solid 10px "+item,padding:"10px"}}>
+            {
+              colorsArrOut = colors.map((item, index) => { 
+                return (n-1) &&<div key={index} style={{border:"solid 10px "+item,padding:"10px"}}>
+                    (n==(this.props.colors.length - 1))&& {this.props.children}
+                    </div>
+            }
+              )
+            }                             
+            </div>          
+        })          
+      })
+*/
+
+
+
+
       return(
-        <div >
-        {colorsArrOut}
-        </div>     
+        <div>   
+          {colorsArrOut} 
+        </div>             
        )
     }
   
