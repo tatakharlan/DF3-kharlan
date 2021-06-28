fc96d15bf30dd2b404fe632d67ae3af6f43d3e5e
TataKharlan <tatakharlan@gmail.com>
2021-06-28 05:16:18 +0300

    rainbfrm methods

--- RainbowFrame/components/RainbowFrame.js
+++ RainbowFrame/components/RainbowFrame.js
@@ -2,7 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import './RainbowFrame.css';
-import Frame from './Frame';
 
 class RainbowFrame extends React.Component {
 
@@ -10,12 +9,12 @@ class RainbowFrame extends React.Component {
         colors: PropTypes.array,      
     };
     
-    
+    /*
     render() {
       let colors = this.props.colors;
       
-      let FrameCode = colors.reduce((el, a, index ) =>(              
-        <Frame  key={index} color={a}>{el}</Frame>
+      let FrameCode = colors.reduce((el, color, index ) =>(              
+        <div key={index} style={{border:"solid 10px "+color,padding:"10px"}}>{el}</div> 
       ),this.props.children);
                        
       return(
@@ -24,7 +23,30 @@ class RainbowFrame extends React.Component {
         </div>             
        )
     }
-  
+    
+    render() {
+      let colors = this.props.colors;
+      let FrameCode = this.props.children;
+      colors.forEach(color => {
+        FrameCode = <div key={color} style={{border:"solid 10px "+color,padding:"10px"}}>{FrameCode}</div>
+      });
+      return(
+        <div>   
+          {FrameCode} 
+        </div>             
+       )
+    }
+  */
+    render() {
+      let colors = this.props.colors;
+        if(colors.length == 0) {
+          return this.props.children;
+        }else {
+          return <div style={{border:"solid 10px "+colors[0],padding:"10px"}}> <RainbowFrame colors={colors.slice(1)}>{this.props.children}</RainbowFrame></div>
+        };
+      
+    }
+
   }
 
   export default RainbowFrame;
