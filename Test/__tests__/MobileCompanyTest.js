32aea246ad7ef0fa5dea8708b7df921ddb3d1c32
TataKharlan <tatakharlan@gmail.com>
2021-08-06 09:47:17 +0300

    test warn

---
+++ Test/__tests__/MobileCompanyTest.js
@@ -0,0 +1,26 @@
+﻿"use strict";
+
+import React from 'react';
+import renderer from 'react-test-renderer';
+
+import MobileCompanyModule from '../modules/MobileCompanyModule';
+
+test('работа MobileCompanyModule', () => {
+
+
+    const component = renderer.create(
+        <MobileCompanyModule />
+      );
+
+    let componentTree=component.toJSON();
+    expect(componentTree).toMatchSnapshot();
+
+    const addUser = component.root.find( el => el.value=='Добавить'); 
+    
+    addUser.props.onClick();
+
+    componentTree=component.toJSON();
+    expect(componentTree).toMatchSnapshot();
+
+
+});
